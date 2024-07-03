f170cf53e86ece19c64531099f9188045076f129
James <thejamesm@users.noreply.github.com>
2024-07-03 21:14:48 +0100

    Implement OOP version

--- calculator.js
+++ calculator.js
@@ -1,73 +1,79 @@
-function reset_calc() {
-    readout.value = 0;
-    startNewNumber = true;
-    prevOperator = "+";
-    prevEquals = false;
-    prevNumber = 0;
-    newNumber = 0;
+class Calculator {
+
+    constructor() {
+        this.startNewNumber = true;
+        this.prevOperator = "+";
+        this.prevEquals = false;
+        this.prevNumber = 0;
+        this.newNumber = 0;
+        this.readout = document.getElementById("readout");
+    }
+
+    reset_calc() {
+        this.readout.value = 0;
+        this.startNewNumber = true;
+        this.prevOperator = "+";
+        this.prevEquals = false;
+        this.prevNumber = 0;
+        this.newNumber = 0;
+    }
+
+    input_digit(digit) {
+        if (this.prevEquals) {
+            this.reset_calc();
+        }
+        if (this.startNewNumber || this.readout.value == "0") {
+            this.readout.value = "";
+        }
+        this.readout.value += digit;
+        this.startNewNumber = false;
+    }
+
+    input_point() {
+        if (this.prevEquals) {
+            this.reset_calc();
+        }
+        if (this.readout.value.length == 0 || this.startNewNumber) {
+            this.readout.value = 0;
+        } else if (this.readout.value.indexOf(".") >= 0) {
+            return;
+        }
+        this.readout.value += "."
+        this.startNewNumber = false;
+    }
+
+    input_operator(operator) {
+        if (!this.startNewNumber && !this.prevEquals) {
+            this.calculate();
+        }
+        this.prevEquals = false;
+        this.prevOperator = operator; 
+        this.startNewNumber = true;
+    }
+
+    calculate() {
+        if (!this.prevEquals) {
+            this.newNumber = Number(this.readout.value);
+        }
+        switch (this.prevOperator) {
+            case "+":
+                this.prevNumber += this.newNumber;
+                break;
+            case "-":
+                this.prevNumber -= this.newNumber;
+                break;
+            case "*":
+                this.prevNumber *= this.newNumber;
+                break;
+            case "/":
+                this.prevNumber /= this.newNumber;
+                break;
+        }
+        this.readout.value = Number(this.prevNumber.toPrecision(12));  // Round without unnecessary zeroes
+        this.prevEquals = true;
+        this.startNewNumber = true;
+    }
+
 }
 
-function input_digit(digit) {
-    if (prevEquals) {
-        reset_calc();
-    }
-    if (startNewNumber || readout.value == "0") {
-        readout.value = "";
-    }
-    readout.value += digit;
-    startNewNumber = false;
-}
-
-function input_point() {
-    if (prevEquals) {
-        reset_calc();
-    }
-    if (readout.value.length == 0 || startNewNumber) {
-        readout.value = 0;
-    } else if (readout.value.indexOf(".") >= 0) {
-        return;
-    }
-    readout.value += "."
-    startNewNumber = false;
-}
-
-function input_operator(operator) {
-    if (!startNewNumber && !prevEquals) {
-        calculate();
-    }
-    prevEquals = false;
-    prevOperator = operator; 
-    startNewNumber = true;
-}
-
-function calculate() {
-    if (!prevEquals) {
-        newNumber = Number(readout.value);
-    }
-    switch (prevOperator) {
-        case "+":
-            prevNumber += newNumber;
-            break;
-        case "-":
-            prevNumber -= newNumber;
-            break;
-        case "*":
-            prevNumber *= newNumber;
-            break;
-        case "/":
-            prevNumber /= newNumber;
-            break;
-    }
-    readout.value = Number(prevNumber.toPrecision(12));  // Round without unnecessary zeroes
-    prevEquals = true;
-    startNewNumber = true;
-}
-
-/* I'd usually avoid global variables, but since we haven't
-   covered classes in JS yet I've resorted to them here. */
-let startNewNumber = true;
-let prevOperator = "+";
-let prevEquals = false;
-let prevNumber = 0;
-let newNumber = 0;
-let readout = document.getElementById("readout");
+const calc = new Calculator();
